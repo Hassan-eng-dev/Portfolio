7956dd8e913e129530f439fd8a858a505abbe52c
Hassan <Hassan.adel3886@gmail.com>
2026-07-06 13:55:39 +0300

    review test

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -12,7 +12,7 @@ const {
   fetchFeaturedProjects(client, 6),
 );
 
-const stats = [
+const statsasdasdas = [
   { value: "40+", label: "Projects delivered" },
   { value: "6", label: "Years of craft" },
   { value: "5", label: "Disciplines covered" },
